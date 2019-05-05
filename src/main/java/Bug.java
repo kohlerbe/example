3ca374362ba2d8db21325a2aaf07837819cad5fc
bernh <bernhardkohler@gmx.de>
2019-05-05 22:31:50 +0200

    lösung

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -1,18 +1,28 @@
 public class Bug {
 	/**
 	 * Find the minimum in the referenced array.
+	 *
 	 * @param a Array of int values
 	 * @return smallest value on the array
 	 * @throws IllegalArgumentException if referenced array is null or empty
 	 */
 	public static int minimum(int[] a) throws IllegalArgumentException {
-		int m = a[0];
-		for (int i = 1; i < a.length; i++) {
-			if (a[i] > m) {
-				m = a[i];
+
+		if (a == null || a.length == 0) {
+
+			throw new IllegalArgumentException();
+
+		} else {
+			int m = a[0];
+			for (int i = 1; i < a.length; i++) {
+				if (a[i] < m) {
+					m = a[i];
+				}
 			}
+
+			return m;
 		}
 
-		return m;
 	}
+
 }
